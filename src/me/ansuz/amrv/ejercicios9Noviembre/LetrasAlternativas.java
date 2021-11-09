0c19397a1c38c78682a6dca942cc20cf428b8c95
marruiad <marruiad@lab03-17.gregoriofer.local>
2021-11-09 12:13:37 +0100

    ejercicios 9/11 y comentarios

--- src/me/ansuz/amrv/ejercicios9Noviembre/LetrasAlternativas.java
+++ src/me/ansuz/amrv/ejercicios9Noviembre/LetrasAlternativas.java
@@ -20,10 +20,12 @@ public class LetrasAlternativas {
     private static final Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        // Declaracion de variables
         String nombre = "";
         String alternado = "";
         int longitud = 0;
+
+        // Introducir texto y comprobar que no es un texto vacio
         do {
             System.out.println("Escribe la palabra que será alternado");
             nombre = scan.nextLine();
@@ -31,20 +33,23 @@ public class LetrasAlternativas {
             if (nombre.length() <= 0) {
                 System.out.println("La palabra que has introducido no es valida");
             }
-            
+
         } while (nombre.length() <= 0);
-        
-        longitud = nombre.length()-1;
-        
+
+        // Ajustar la longitud para facilitar el uso
+        longitud = nombre.length() - 1;
+
+        // Imprmir caracteres
         for (int i = 0; i < nombre.length() / 2; i++) {
             alternado += nombre.charAt(i);
-            alternado += nombre.charAt(longitud-i);
+            alternado += nombre.charAt(longitud - i);
         }
 
+        // Arreglo para que si la cadena es impar introduzca el ultimo caracter correctamente
         if (nombre.length() % 2 != 0) {
             alternado += nombre.charAt(longitud / 2);
         }
-        
+
         System.out.println("El texto alternativo es " + alternado);
     }
 }
